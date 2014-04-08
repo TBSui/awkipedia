29773e3e9de95faf69035abc99c0ff08e210a738
Tianbo <tianbosui@gmail.com>
2014-04-08 16:17:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awkipidia::Application.config.secret_key_base = 'bf7d6bc18d37d38b6c96a4dfba828b86a33782aea68e87933b1812b80c7e22c90c4226afdce7b11ff54a1a2916af4590e465e5976c574378f93a69527cb8e28f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Awkipidia::Application.config.session_store :cookie_store, key: '_awkipidia_session'
