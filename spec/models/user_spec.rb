d0682834ba62e1087958fcca33c1e155c6270bc1
Corey Drees <cdrees@wisc.edu>
2014-04-28 12:14:55 -0500

    added password feature

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,15 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+                            password: "user123", password_confirmation: "user123") }
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -21,5 +25,18 @@ describe User do
   describe "when email is not present" do
   	before { @user.email = " " }
   	it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+  
+  describe "when passwords do not match" do
+    before { @user.password_confirmation = "user321" }
+    it { should_not be_valid }
   end
 end
